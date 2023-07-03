c4c3e2984f5138f44c81ea13471d22d251c70156
jecode93 <jeanemmanuelcadet@gmail.com>
2023-07-03 17:15:16 -0400

    Implement the ui and the function of the website

--- js/add-books.js
+++ js/add-books.js
@@ -1,16 +1,14 @@
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const add = document.querySelector('.submit');
-const all_books = document.querySelector('#all-books');
+const allBooks = document.querySelector('#all-books');
 const error = document.querySelector('.error');
-
-
 
 const bookStore = JSON.parse(localStorage.getItem('books')) || [];
 
-function showBooks(){
-for(let i = 0; i < bookStore.length; i++){
-  all_books.innerHTML += `
+function showBooks() {
+for (let i = 0; i < bookStore.length; i += 1) {
+  allBooks.innerHTML += `
   <article class="books">
     <p>${bookStore[i].title}</p>
     <p>${bookStore[i].author}</p>
@@ -19,31 +17,31 @@ for(let i = 0; i < bookStore.length; i++){
   </article>
 `
 };
-}
+};
 
-function addBooks(event){
-  event.preventDefault();
-  if(title.value && author.value){
-    const newBooks = {
-        title: title.value,
-        author: author.value,
-      };
-    bookStore.push(newBooks);
-	  localStorage.setItem('books', JSON.stringify(bookStore));
-    showBooks();
-    title.value = '';
-    author.value = '';
-    error.innerHTML = '';
-  }else{
-    error.innerHTML = 'Please enter a book'
+  function addBooks(event) {
+    event.preventDefault();
+    if(title.value && author.value) {
+      const newBooks = {
+          title: title.value,
+          author: author.value,
+        };
+      bookStore.push(newBooks);
+	    localStorage.setItem('books', JSON.stringify(bookStore));
+      showBooks();
+      title.value = '';
+      author.value = '';
+      error.innerHTML = '';
+    }else{
+      error.innerHTML = 'Please enter a book'
+    }
   }
-}
 
-function removeBook(index) {
-  bookStore.splice(index, 1);
-  localStorage.setItem('books', JSON.stringify(bookStore));
-  showBooks();
-}
+  function removeBook(index) {
+    bookStore.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(bookStore));
+    showBooks();
+  }
 
 add.addEventListener('click', addBooks);
 showBooks();
