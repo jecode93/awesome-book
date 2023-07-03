e0470b71619a0d8250bb530fd408900e33098ccd
jecode93 <jeanemmanuelcadet@gmail.com>
2023-07-03 17:32:21 -0400

    Make change as requested by Linters

--- js/add-books.js
+++ js/add-books.js
@@ -6,7 +6,7 @@ const error = document.querySelector('.error');
 
 const bookStore = JSON.parse(localStorage.getItem('books')) || [];
 
-  function showBooks() {
+function showBooks() {
   for (let i = 0; i < bookStore.length; i += 1) {
     allBooks.innerHTML += `
     <article class="books">
@@ -16,32 +16,32 @@ const bookStore = JSON.parse(localStorage.getItem('books')) || [];
       <hr>
     </article>
 `;
+  }
+}
+
+function addBooks(event) {
+  event.preventDefault();
+  if (title.value && author.value) {
+    const newBooks = {
+      title: title.value,
+      author: author.value,
     };
-  }
-
-  function addBooks(event) {
-    event.preventDefault();
-    if (title.value && author.value) {
-      const newBooks = {
-        title: title.value,
-        author: author.value,
-      };
-      bookStore.push(newBooks);
-	    localStorage.setItem('books', JSON.stringify(bookStore));
-      showBooks();
-      title.value = '';
-      author.value = '';
-      error.innerHTML = '';
-    }else{
-      error.innerHTML = 'Please enter a book'
-    }
-  }
-
-  function removeBook(index) {
-    bookStore.splice(index, 1);
+    bookStore.push(newBooks);
     localStorage.setItem('books', JSON.stringify(bookStore));
     showBooks();
+    title.value = '';
+    author.value = '';
+    error.innerHTML = '';
+  } else {
+    error.innerHTML = 'Please enter a book'
   }
+}
+
+function removeBook(index) {
+  bookStore.splice(index, 1);
+  localStorage.setItem('books', JSON.stringify(bookStore));
+  showBooks();
+}
 
 add.addEventListener('click', addBooks);
 showBooks();
